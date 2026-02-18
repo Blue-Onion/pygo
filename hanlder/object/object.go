5a0a003c366085c2cde33f9fd8d86d2a3914fc04
Blue-Onion <rawatadityasingh963@gmail.com>
2026-02-18 22:35:28 +0530

    Object Strcut

--- hanlder/object/object.go
+++ hanlder/object/object.go
@@ -1,1 +1,33 @@
 package object
+
+import (
+	"github.com/Blue-Onion/pygo/hanlder/repo"
+)
+
+type GitObject interface {
+	Serialize() ([]byte, error)
+	Deserialize([]byte) error
+	Type() string
+}
+type Blob struct {
+	Data []byte
+}
+
+func (b *Blob) Serialize() ([]byte, error) {
+	return b.Data, nil
+}
+
+func (b *Blob) Deserialize(raw []byte) error {
+	b.Data = raw
+	return nil
+}
+
+func (b *Blob) Type() string {
+	return "blob"
+}
+func ObjectRead(repo *repo.Gitrepo)  {
+	
+}
+func ObjectWrite(repo *repo.Gitrepo){
+	
+}
